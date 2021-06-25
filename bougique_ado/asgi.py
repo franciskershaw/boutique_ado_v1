6ad31b4aadd43341956613367628a4bc5ff46aee
franciskershaw <franciskershaw2004@hotmail.com>
2021-06-25 08:41:46 +0000

    initial commit

---
+++ bougique_ado/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bougique_ado project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bougique_ado.settings')
+
+application = get_asgi_application()
