12783c60332c43b652f3bf8e8a149faf8ac572d4
franciskershaw <franciskershaw2004@hotmail.com>
2021-07-02 10:24:48 +0000

    add toasts

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -58,7 +59,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """A view to show product details"""
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
