cf7354013c3ea8c73164d9843d5aa10e0dc0a4db
franciskershaw <franciskershaw2004@hotmail.com>
2021-07-02 10:29:10 +0000

    fix gitpod issues

--- bag/urls.py
+++ bag/urls.py
@@ -4,9 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
-<<<<<<< HEAD
     path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
     path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
-=======
->>>>>>> 875e93c4b580e927e5ab8bdc76dce70f2a622ef0
 ]
